f2d7813d30ed86200161badddfb1e5816654d76e
Yuri Fialho <fialho.yuri@eb.mil.br>
2022-03-14 18:53:07 -0300

    Added Web Capability.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.9
 
 
-COPY . /home/ruleragent/.
-WORKDIR /home/ruleragent
+COPY . /usr/src/app/.
+WORKDIR /usr/src/app
 RUN python -m pip install -r requirements.txt
 
 CMD [ "python", "main.py"]
--- main.py
+++ main.py
@@ -1,54 +1,43 @@
 import time
 import asyncio
 import sys
+import logging
+from spade import quit_spade
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.template import Template
 
 class RulerAgent(Agent):
-    class MyBehav(CyclicBehaviour):
-        async def on_start(self):
-            print("Starting behaviour . . .", file = sys.stdout)
-            self.counter = 0
-
+    class ReceiveMsgCyclicBehaviour(CyclicBehaviour):
         async def run(self):
-            print("Counter: {}".format(self.counter), file = sys.stdout)
-            self.counter += 1
-            await asyncio.sleep(1)
+            logging.info("Receiving msg...")
 
-    async def setup(self):
-        print("Agent starting . . .", file = sys.stdout)
-        self.b = self.MyBehav()
-        self.a = self.RecevyInformBehav()
-
-        self.add_behaviour(self.b)
-        temp = Template()
-        temp.set_metadata("performative", "inform")
-        self.add_behaviour(self.a, template=temp)
-
-    class RecevyInformBehav(OneShotBehaviour):
-        async def run(self):
-            print("Receiving msg...")
-
-            msg = None
-            while not msg:
-                msg = await self.receive(timeout=10)
-                print(msg.body)
-
+            msg = await self.receive(timeout=10)
+            if msg:
+                logging.info("Message received with content: {}".format(msg.body))
+        
+        async def on_end(self):
             await self.agent.stop()
+    
+    async def setup(self):
+        a = self.ReceiveMsgCyclicBehaviour()
+        self.add_behaviour(a)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     # prosodyctl register ruleragent prosody-server ruleragent
-    dummy = RulerAgent("ruleragent@prosody-server", "ruleragent")
-    future = dummy.start()
+    ruleragent = RulerAgent("ruleragent@xmpp-server", "ruleragent")
+    future = ruleragent.start(auto_register=True)
+    ruleragent.web.start(hostname='ruleragent', port='10001')
     future.result()
 
-    print("Wait until user interrupts with ctrl+C", file = sys.stdout)
-    while dummy.is_alive():
+    logging.info("Wait until user interrupts with ctrl+C")
+    while ruleragent.is_alive():
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            print("Stopping...", file = sys.stdout)
-            dummy.stop()
+            logging.info("Stopping...")
+            ruleragent.stop()
             break
+    quit_spade()
